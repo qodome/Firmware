9b1ff9928b1b9d016c7f61ad9a12b219a4d1afac
orcguru <wangting@gmail.com>
2014-10-13 07:59:27 +0800

    PIC16 sampling && nRF51822 bulk_req detection

---
+++ IM14BEA/PIC16F1788/ecg_buffer_mgr.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=ecg_buffer_mgr.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/ecg_buffer_mgr.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=ecgbuffermgr.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/ecgbuffermgr.x.tar
