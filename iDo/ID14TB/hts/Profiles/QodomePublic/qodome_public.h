90d81e8fad6690273d504b4a4e5ad20a05a166ca
orcguru <wangting@gmail.com>
2015-01-26 16:51:01 +0800

    [iDo] Update Qodome service UUID

--- iDo/ID14TB/hts/Profiles/QodomePublic/qodome_public.h
+++ iDo/ID14TB/hts/Profiles/QodomePublic/qodome_public.h
@@ -4,8 +4,8 @@
 #ifndef __QODOME_PUBLIC__
 #define __QODOME_PUBLIC__
 
-#define QODOME_PUBLIC_SERVICE               0xBEEF
-#define QODOME_PUBLIC_SET_NAME              0xBE01
+#define QODOME_PUBLIC_SERVICE               0x90D0
+#define QODOME_PUBLIC_SET_NAME              0x9001
 
 bStatus_t QodomePublic_AddService(void);
 
